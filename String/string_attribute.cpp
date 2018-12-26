2430535ef5a11df3b6da539214af12027c0e8276
srhuang <lukyandy3162@gmail.com>
2018-12-27 03:30:12 +0800

    update string

--- String/string_attribute.cpp
+++ String/string_attribute.cpp
@@ -15,8 +15,38 @@ using namespace std;
 #include <string>
 
 /*==============================================================*/
+
+void printAttribute(const string &);
+
 int main(){
-	
+	string string1;
+
+	cout << "Before Input: \n" << boolalpha;
+	printAttribute(string1);
+
+	string1="Test"; //4
+
+	cout << "\nAfter Input: " << string1 << endl;
+	printAttribute(string1);
+
+	string1 += " 123456789"; //1+9
+
+	cout << "\nAfter Input: " << string1 << endl;
+	printAttribute(string1);
+
+	string1 += " abcdefghijklmnopqrstuvwxyz"; //1+26
+
+	cout << "\nAfter Input: " << string1 << endl;
+	printAttribute(string1);
 
 	return 0;
+}
+
+void printAttribute(const string &stringRef)
+{
+	cout << "capacity: " << stringRef.capacity()
+		<< "\nempty: " << stringRef.empty()
+		<< "\nlength:" << stringRef.length()
+		<< "\nmax_size: " << stringRef.max_size()
+		<< "\nsize: " << stringRef.size() << endl;
 }
---
+++ String/string_find_replace.cpp
@@ -0,0 +1,57 @@
+/*****************************************************************
+Name	:string_find_replace
+Author	:srhuang
+Email	:lukyandy3162@gmail.com
+History	:
+	20181226 Initial Version
+http://www.cplusplus.com/reference/string/string/find/
+http://www.cplusplus.com/reference/string/string/rfind/
+http://www.cplusplus.com/reference/string/string/find_first_of/
+http://www.cplusplus.com/reference/string/string/find_last_of/
+http://www.cplusplus.com/reference/string/string/find_first_not_of/
+http://www.cplusplus.com/reference/string/string/find_last_not_of/
+*****************************************************************/
+#include <iostream>
+using namespace std;
+#include <string>
+
+/*==============================================================*/
+int main(){
+	/* 
+	 * Find
+	 */
+	string string1("Dream as you will live forever, and live as you will die today.");
+	cout << "String : " << string1 << "(" << string1.length() << ")" <<endl;
+
+	//find "as"
+	cout << "find as : " << string1.find("as") << endl;
+	cout << "rfind as : " << string1.rfind("as") << endl;
+
+	int location;
+	//find 'e'
+	location = string1.find_first_of("ey");
+	cout << "find_first_of ey : " << location << endl;
+
+	//find 'y'
+	location = string1.find_last_of("ey");
+	cout << "find_last_of ey : " << location << endl;
+
+	location = string1.find_first_not_of("abcdeABCDEr");
+	cout << "find_first_not_of abcdeABCDE : " << location << endl;
+
+	location = string1.find_last_not_of("abcdeABCDEy.");
+	cout << "find_last_not_of abcdeABCDE : " << location << endl;
+
+	//Not find
+	string string2("asdf");
+	cout << "\nString 2 : " << string2 << endl;
+	location = string2.find_first_of("gh");
+	cout << "find_first_of gh : " << location << endl;
+
+	/* 
+	 * Replace
+	 */
+	
+	
+	return 0;
+}
