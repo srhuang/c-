677cdf57fbb7c01ef057029bdadd83af1293d165
srhuang <lukyandy3162@gmail.com>
2019-09-30 02:11:12 +0800

    update BT traversal

--- Tree/BT_from_array.cpp
+++ Tree/BT_from_array.cpp
@@ -3,11 +3,12 @@ Name    :BT_from_array
 Author  :srhuang
 Email   :lukyandy3162@gmail.com
 History :
-    20181225 Initial Version
+    20190927 Initial Version
 *****************************************************************/
 #include <iostream>
 #include <sstream>
 #include <queue>
+#include <stack>
 using namespace std;
 /*==============================================================*/
 
@@ -25,9 +26,7 @@ private:
 public:
     TreeNode *root;
     BinaryTree(const char *str);
-    void preorderTraversal(TreeNode *parent);
-    void inorderTraversal(TreeNode *parent);
-    void postorderTraversal(TreeNode *parent);
+
 };
 
 BinaryTree::BinaryTree(const char *str)
@@ -48,7 +47,7 @@ void BinaryTree::LevelorderConstruct(stringstream &ss)
     char input;
 
     while(ss >> input){
-        cout << "input=" << input << endl;
+        //cout << "input=" << input << endl;
 
         //handle the left child
         if(input != 'x'){
@@ -72,60 +71,12 @@ void BinaryTree::LevelorderConstruct(stringstream &ss)
     }
 }
 
-void BinaryTree::preorderTraversal(TreeNode *parent)
-{
-    if(NULL == parent){
-        return;
-    }
-
-    //do the right things
-    cout << " " << parent->val;
-    preorderTraversal(parent->left);
-    preorderTraversal(parent->right);
-}
-
-void BinaryTree::inorderTraversal(TreeNode *parent)
-{
-    if(NULL == parent){
-        return;
-    }
-
-    inorderTraversal(parent->left);
-    //do the right things
-    cout << " " << parent->val;
-    inorderTraversal(parent->right);
-}
-
-void BinaryTree::postorderTraversal(TreeNode *parent)
-{
-    if(NULL == parent){
-        return;
-    }
-
-    postorderTraversal(parent->left);
-    postorderTraversal(parent->right);
-    //do the right things
-    cout << " " << parent->val;
-}
-
 int main(int argc, char const *argv[]){
     const char *input = "1 2 3 x x 4 5 6 7";
     BinaryTree BT(input);
 
     TreeNode *temp = BT.root->right;
     cout << "test : " << temp->right->val << endl;
-
-    cout << "inorder :";
-    BT.inorderTraversal(BT.root);
-    cout << endl;
-
-    cout << "preorder :";
-    BT.preorderTraversal(BT.root);
-    cout << endl;
-
-    cout << "postorder :";
-    BT.postorderTraversal(BT.root);
-    cout << endl;
 
     return 0;
 }
---
+++ Tree/BT_traversal.cpp
@@ -0,0 +1,294 @@
+/*****************************************************************
+Name    :BT_traversal
+Author  :srhuang
+Email   :lukyandy3162@gmail.com
+History :
+    20190930 Initial Version
+*****************************************************************/
+#include <iostream>
+#include <sstream>
+#include <queue>
+#include <stack>
+using namespace std;
+/*==============================================================*/
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(-1), left(NULL), right(NULL) {}
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class BinaryTree{
+private:
+    void LevelorderConstruct(stringstream &ss);
+public:
+    TreeNode *root;
+    BinaryTree(const char *str);
+
+    void preorderTraversalRecursion(TreeNode *parent);
+    void inorderTraversalRecursion(TreeNode *parent);
+    void postorderTraversalRecursion(TreeNode *parent);
+    void levelorderTraversalIteration(TreeNode *parent);
+
+    void preorderTraversalIteration(TreeNode *parent);
+    void inorderTraversalIteration(TreeNode *parent);
+    void postorderTraversalIteration(TreeNode *parent);
+    void printGivenLevel(TreeNode *parent, int level);
+    int height(TreeNode *parent);
+    void levelorderTraversalRecursion(TreeNode *parent);
+
+};
+
+BinaryTree::BinaryTree(const char *str)
+{
+    stringstream ss;
+    ss << str;
+
+    root = new TreeNode;
+    ss >> root->val;
+
+    LevelorderConstruct(ss);
+}
+
+void BinaryTree::LevelorderConstruct(stringstream &ss)
+{
+    queue<TreeNode*> q;
+    TreeNode *current = root;
+    char input;
+
+    while(ss >> input){
+        cout << "input=" << input << endl;
+
+        //handle the left child
+        if(input != 'x'){
+            TreeNode *newNode = new TreeNode(input-'0');
+            current->left = newNode;
+            q.push(newNode);
+        }
+
+        if(!(ss >> input)){
+            break;
+        }
+        
+        if(input != 'x'){
+            TreeNode *newNode = new TreeNode(input-'0');
+            current->right = newNode;
+            q.push(newNode);
+        }
+
+        current = q.front();
+        q.pop();
+    }
+}
+
+void BinaryTree::preorderTraversalRecursion(TreeNode *parent)
+{
+    if(NULL == parent){
+        return;
+    }
+
+    //do the right things
+    cout << " " << parent->val;
+    preorderTraversalRecursion(parent->left);
+    preorderTraversalRecursion(parent->right);
+}
+
+void BinaryTree::inorderTraversalRecursion(TreeNode *parent)
+{
+    if(NULL == parent){
+        return;
+    }
+
+    inorderTraversalRecursion(parent->left);
+    //do the right things
+    cout << " " << parent->val;
+    inorderTraversalRecursion(parent->right);
+}
+
+void BinaryTree::postorderTraversalRecursion(TreeNode *parent)
+{
+    if(NULL == parent){
+        return;
+    }
+
+    postorderTraversalRecursion(parent->left);
+    postorderTraversalRecursion(parent->right);
+    //do the right things
+    cout << " " << parent->val;
+}
+
+void BinaryTree::levelorderTraversalIteration(TreeNode *parent)
+{
+    queue<TreeNode*> q;
+    q.push(parent);
+
+    while(!q.empty()){
+        TreeNode *current = q.front();
+        q.pop();
+        cout << " " << current->val;
+
+        if(current->left)   q.push(current->left);
+        if(current->right)  q.push(current->right);
+    }//while
+}
+
+void BinaryTree::inorderTraversalIteration(TreeNode *parent)
+{
+    stack<TreeNode*> st;
+    TreeNode *current = parent;
+
+    while(current != NULL || !st.empty()){
+        //reach the leftmost node of the current node
+        while(current != NULL){
+            st.push(current);
+            current = current->left;
+        }
+
+        current = st.top();
+        st.pop();
+
+        cout << " " << current->val;
+
+        current = current->right;
+    }
+}
+
+void BinaryTree::preorderTraversalIteration(TreeNode *parent)
+{
+    if(NULL == parent){
+        return;
+    }
+
+    stack<TreeNode*> st;
+    st.push(parent);
+
+    while(!st.empty()){
+        TreeNode *current = st.top();
+        st.pop();
+
+        cout << " " << current->val;
+
+        if(current->right)
+            st.push(current->right);
+        if(current->left)
+            st.push(current->left);
+    }
+}
+
+void BinaryTree::postorderTraversalIteration(TreeNode *parent)
+{
+    if(NULL == parent){
+        return;
+    }
+
+    stack<TreeNode*> st;
+    do{
+        //move to leftmost node
+        while(parent){
+            //cout << "parent : " <<parent->val << endl;
+            if(parent->right){
+                st.push(parent->right);
+            }
+            st.push(parent);
+
+            parent = parent->left;
+        }
+
+        //pop the stack and handle with it
+        parent = st.top();
+        st.pop();
+
+        if(parent->right && !st.empty() && (st.top()==parent->right)){
+            //handl the right child first
+            st.pop();
+            st.push(parent);
+            parent = parent->right;
+        }else{
+            //do the right thing
+            cout << " " << parent->val;
+            parent = NULL; //let next iteration only pop the stack
+        }
+    }while(!st.empty());
+}
+
+void BinaryTree::printGivenLevel(TreeNode *parent, int level)
+{
+    if(NULL == parent){
+        return;
+    }
+
+    if(1 == level){
+        cout << " " << parent->val;
+    }else if(level > 1){
+        printGivenLevel(parent->left, level-1);
+        printGivenLevel(parent->right, level-1);
+    }
+}
+
+int BinaryTree::height(TreeNode *parent)
+{
+    if(NULL == parent){
+        return 0;
+    }
+
+    int left_height = height(parent->left);
+    int right_height = height(parent->right);
+
+    if(left_height > right_height)
+        return (left_height+1);
+    else
+        return (right_height+1);
+}
+
+void BinaryTree::levelorderTraversalRecursion(TreeNode *parent)
+{
+    int h = height(parent);
+    for(int i=1; i<=h; i++){
+        printGivenLevel(parent, i);
+    }
+}
+
+int main(int argc, char const *argv[]){
+    const char *input = "1 2 3 x x 4 5 6 7";
+    BinaryTree BT(input);
+
+    TreeNode *temp = BT.root->right;
+    cout << "test : " << temp->right->val << endl;
+
+    cout << "inorder :";
+    BT.inorderTraversalRecursion(BT.root);
+    cout << endl;
+
+    cout << "preorder :";
+    BT.preorderTraversalRecursion(BT.root);
+    cout << endl;
+
+    cout << "postorder :";
+    BT.postorderTraversalRecursion(BT.root);
+    cout << endl;
+
+    cout << "levelorder :";
+    BT.levelorderTraversalIteration(BT.root);
+    cout << endl;
+
+    cout << "inorder iteration :";
+    BT.inorderTraversalIteration(BT.root);
+    cout << endl;
+
+    cout << "preorder iteration :";
+    BT.preorderTraversalIteration(BT.root);
+    cout << endl;
+
+    cout << "postorder iteration :";
+    BT.postorderTraversalIteration(BT.root);
+    cout << endl;
+
+    cout << "levelorder :";
+    BT.levelorderTraversalRecursion(BT.root);
+    cout << endl;
+
+    return 0;
+}
+/*==============================================================*/
