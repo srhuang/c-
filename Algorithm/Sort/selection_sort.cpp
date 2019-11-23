8901eb690f3c2acbf870386d6a121d762fed8dcc
srhuang <lukyandy3162@gmail.com>
2019-11-23 19:39:22 +0800

    Create selection_sort.cpp

---
+++ Algorithm/Sort/selection_sort.cpp
@@ -0,0 +1,134 @@
+/*****************************************************************
+Name    :selection_sort
+Author  :srhuang
+Email   :lukyandy3162@gmail.com
+History :
+    20191123 Initial Version
+*****************************************************************/
+#include <iostream>
+using namespace std;
+using namespace std::chrono; 
+
+/*==============================================================*/
+//Global area
+
+/*==============================================================*/
+//Function area
+int *random_case(int number)
+{
+    int *result = new int[number];
+
+    //generate index ordered arrary
+    for(int i=0; i<number; i++){
+        result[i]=i+1;
+    }
+
+    //swap each position
+    srand(time(NULL));
+    for(int i=0; i<number-1; i++){
+        int j = i + rand() / (RAND_MAX / (number-i));
+        //swap
+        int t=result[i];
+        result[i] = result[j];
+        result[j]=t;
+    }
+
+    return result;
+}
+
+int *descending_case(int number)
+{
+    int *result = new int[number];
+
+    //generate index ordered arrary
+    for(int i=0; i<number; i++){
+        result[i]=number-i;
+    }
+
+    return result;
+}
+
+int *ascending_case(int number)
+{
+    int *result = new int[number];
+
+    //generate index ordered arrary
+    for(int i=0; i<number; i++){
+        result[i]=i+1;
+    }
+
+    return result;
+}
+
+void selection_sort(int *input, int n)
+{
+    // One by one move boundary of unsorted subarray
+    for(int i=0; i<n; i++){
+
+        // Find the minimum element in unsorted array
+        int min_index = i;
+        for(int j=i+1; j<n; j++){
+            if(input[j] < input[min_index]){
+                min_index = j;
+            }
+        }
+
+        // Swap the found minimum element
+        int t = input[i];
+        input[i] = input[min_index];
+        input[min_index] = t;
+    }
+}
+
+
+/*==============================================================*/
+int main(int argc, char const *argv[]){
+    int n=10000;
+
+    //generate data
+    int *ascending_data = ascending_case(n);
+    int *descending_data = descending_case(n);
+    int *random_data = random_case(n);
+
+    /*
+    cout << "Before sorting :";
+    for(int i=0; i<n; i++){
+        cout << random_data[i] << " ";
+    }
+    cout << endl;
+    */
+
+    //sort
+    auto start = high_resolution_clock::now(); 
+    selection_sort(ascending_data, n);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start); 
+    cout << "Time taken by ascending_data: "
+         << duration.count() << " microseconds" << endl; 
+
+    start = high_resolution_clock::now(); 
+    selection_sort(descending_data, n);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start); 
+    cout << "Time taken by descending_data: "
+         << duration.count() << " microseconds" << endl; 
+
+    start = high_resolution_clock::now(); 
+    selection_sort(random_data, n);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start); 
+    cout << "Time taken by random data: "
+         << duration.count() << " microseconds" << endl; 
+
+    /*
+    cout << "\nAfter sorting :";
+    for(int i=0; i<n; i++){
+        cout << random_data[i] << " ";
+    }
+    cout << endl;
+    */
+     
+    
+    return 0;
+}
+/*==============================================================*/
